52b1214e95129c39adb8a43b28248adba67324d6
Daniel Cumberbatch <appledoughnut@gmail.com>
2016-12-18 11:40:52 +0000

    Restructured project; Updated .gitignore and compile.sh for tests

--- compile.sh
+++ compile.sh
@@ -4,9 +4,11 @@ using_imac=0
 
 if [ $using_imac -eq 0 ]
 then
-    g++ -o main *.cpp --std=c++14 -I/usr/local/boost_installed/include
+    g++ -o bin/main/main src/main/*.cpp --std=c++14 -I/usr/local/boost_installed/include
+    g++ -o bin/test/test src/test/*.cpp --std=c++14 -I/usr/local/boost_installed/include -I/Users/danielcumberbatch/Documents/SoftwareEngineeringPlaypen/googletest/googletest -I/Users/danielcumberbatch/Documents/SoftwareEngineeringPlaypen/googletest/googlemock
 else
-    g++ -o main *.cpp --std=c++14 -I/Developer/Applications/boost_1_62_0/
+    g++ -o bin/main/main src/main/*.cpp --std=c++14 -I/Developer/Applications/boost_1_62_0/
+    g++ -o bin/test/test src/test/*.cpp --std=c++14 -I/Developer/Applications/boost_1_62_0/ -I/Users/danielcumberbatch/Documents/SoftwareEngineeringPlaypen/googletest/googletest -I/Users/danielcumberbatch/Documents/SoftwareEngineeringPlaypen/googletest/googlemock
 fi
 
 exit $?

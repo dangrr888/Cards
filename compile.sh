3994b7b791d994a7da37bb51a9bc72b1b05d1210
Daniel Cumberbatch <appledoughnut@gmail.com>
2017-02-27 00:07:17 +0000

    achieved successful compilation and linkage

--- compile.sh
+++ compile.sh
@@ -1,8 +1,7 @@
 #! /bin/bash -
 
 ROOT_DIR=/Users/danielcumberbatch/Documents/SoftwareEngineeringPlaypen/CrackingTheCodingInterview/Cards
-
-g++ -o bin/main/main src/main/C++/*.cpp --std=c++14 -I/usr/local/boost_installed/include -I$ROOT_DIR/src/main/include
-g++ -o bin/test/test src/test/C++/*.cpp --std=c++14 -I/usr/local/boost_installed/include -I$ROOT_DIR/src/main/include -lgtest -lgmock
+g++ -o bin/main/main src/main/C++/*.cpp src/main/main.cpp --std=c++14 -I/usr/local/boost_installed/include -I$ROOT_DIR/src/main/include
+g++ -o bin/test/test src/test/C++/*.cpp src/main/C++/*.cpp --std=c++14 -I/usr/local/boost_installed/include -I$ROOT_DIR/src/main/include -lgtest -lgmock
 
 exit 0

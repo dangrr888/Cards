ca668d5700b1bd34507bbdec2ca2cb34d1caed0e
Daniel Cumberbatch <appledoughnut@gmail.com>
2017-04-02 15:43:54 +0100

    Deleted main.cpp and added spiketests

--- compile.sh
+++ compile.sh
@@ -1,7 +1,8 @@
 #! /bin/bash -
 
-ROOT_DIR=/Users/danielcumberbatch/Documents/SoftwareEngineeringPlaypen/CrackingTheCodingInterview/Cards
-g++ -o bin/main/main src/main/C++/*.cpp src/main/main.cpp --std=c++14 -I/usr/local/boost_installed/include -I$ROOT_DIR/src/main/include
-g++ -o bin/test/test src/test/C++/*.cpp src/main/C++/*.cpp --std=c++14 -I/usr/local/boost_installed/include -I$ROOT_DIR/src/main/include -lgtest -lgmock
+ROOT_DIR=${PWD}
 
-exit 0
+g++ -o bin/test/test src/test/C++/*.cpp src/main/C++/*.cpp --std=c++14 -I/usr/local/boost_installed/include -I$ROOT_DIR/src/main/include -lgtest -lgmock
+g++ -o bin/main/main src/main/C++/*.cpp src/spiketest/C++/main.cpp --std=c++14 -I/usr/local/boost_installed/include -I$ROOT_DIR/src/main/include
+
+exit $?
